0bd91204b73184a2a3f8a5b9dc7944b032c0a22d
Stepan Jamalayn <stepan.jamalyan@gmail.com>
2021-03-31 20:48:52 +0600

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   )
 }
