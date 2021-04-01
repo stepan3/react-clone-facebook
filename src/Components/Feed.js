8d4e11734a0664c0b6dbae7333c401bf868d4297
Stepan Jamalayn <stepan.jamalyan@gmail.com>
2021-04-01 17:33:21 +0600

    added posts

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,12 +2,20 @@ import React from 'react'
 import MessageSender from './MessageSender'
 import StoryReel from './StoryReel'
 import './Feed.css'
+import Post from './Post'
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic="https://i.pinimg.com/originals/05/fc/57/05fc57385b3678a0549d71f25f5c4189.jpg"
+        message="This is a message"
+        timestamp="1601493943737"
+        imgName="imgName"
+        username="Stepan"
+      />
 
       {/* {postData.map(entry => (
         <Post
